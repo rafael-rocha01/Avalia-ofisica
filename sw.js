de28be1c7018c0cdb9997cb99d543c332bb960e9
rafael-rocha01 <99609805+rafael-rocha01@users.noreply.github.com>
2026-01-14 18:18:02 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "avaliacao-pro-v1";
+const ASSETS = [
+  "./",
+  "./index.html"
+];
+
+// 1. Instalação: Guarda os arquivos no cache
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 2. Ativação: Limpa caches antigos se mudar a versão
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 3. Interceptação: Serve o arquivo offline se não tiver internet
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
